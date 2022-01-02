66e3c0a8e8c2d8943120dd450d27e1b0593c9de3
true.living <68152189+swetha4444@users.noreply.github.com>
2022-01-02 20:42:22 +0530

    Update MainActivity.java

--- AndroidStudio-main/SD card/MainActivity.java
+++ AndroidStudio-main/SD card/MainActivity.java
@@ -6,53 +6,61 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
-    EditText fname,fcont;
-    Button read,write;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fname = (EditText) findViewById(R.id.file_name);
-        fcont = (EditText) findViewById(R.id.file_cont);
-        read = (Button) findViewById(R.id.btn_read);
-        write = (Button) findViewById(R.id.btn_write);
-        read.setOnClickListener(new View.OnClickListener() {
+
+        EditText name = (EditText) findViewById(R.id.user_name);
+        EditText message = (EditText) findViewById(R.id.user_message);
+        Button bt1 = (Button) findViewById(R.id.read);
+        Button bt2 = (Button) findViewById(R.id.write);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try{
-                    String inp="",text="";
-                    File file = new File(getFilesDir(),fname.getText().toString());
-                    BufferedReader bf = new BufferedReader(new FileReader(file));
-                    while((inp=bf.readLine())!=null) text+=inp;
-                    fcont.setText(text);
-                }catch(Exception e){
+                try {
+                    String inp,text = "";
+                    File file = new File(getFilesDir(),name.getEditableText().toString());
+                    BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                    while ((inp = bufferedReader.readLine()) != null) text += inp;
+                    message.setText(text);
+                } catch (FileNotFoundException e) {
+                    Toast.makeText(getBaseContext(),"No such file",Toast.LENGTH_LONG).show();
+                    message.setText("");
+                    e.printStackTrace();
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         });
-        write.setOnClickListener(new View.OnClickListener() {
+
+        bt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try{
-                    String inp="",text="";
-                    File file = new File(getFilesDir(),fname.getText().toString());
+                try {
+                    File file = new File(getFilesDir(),name.getEditableText().toString());
                     FileOutputStream fp = new FileOutputStream(file);
-                    fp.write(fcont.getText().toString().getBytes());
-                    fp.close();
-                    fcont.setText("");
-                }catch(Exception e){
+                    fp.write(message.getEditableText().toString().getBytes(StandardCharsets.UTF_8));
+                    message.setText("");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         });
     }
-
 }
