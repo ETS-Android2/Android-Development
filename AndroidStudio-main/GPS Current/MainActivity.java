a5103ae00f4f8d2f8dabdd83986d4bcbe01b1b80
true.living <68152189+swetha4444@users.noreply.github.com>
2022-01-04 02:55:30 +0530

    Update MainActivity.java

--- AndroidStudio-main/GPS Current/MainActivity.java
+++ AndroidStudio-main/GPS Current/MainActivity.java
@@ -1,79 +1,57 @@
+//Pixel 2 -API 22
 package com.example.gpslocation;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
-    LocationManager lmanager;
-    String provider;
+
+public class MainActivity extends AppCompatActivity implements LocationListener{
+    TextView tv_lat,tv_log;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_lat = findViewById(R.id.lat);
+        tv_log = findViewById(R.id.lon);
 
-        init();
-    }
-
-    private void init() {
-        lmanager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria=new Criteria();
-        provider= lmanager.getBestProvider(criteria,false);
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        if(ActivityCompat.checkSelfPermission(MainActivity.this,ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(MainActivity.this,ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
             return;
 
-        if (provider!=null && !provider.equals("")){
-            Location location= lmanager.getLastKnownLocation(provider);
-            lmanager.requestLocationUpdates(provider, 100, 1, this);
-            if(location!=null){
-                onLocationChanged(location);
-            }
-            else{
-                Toast.makeText(getBaseContext(), "Location not available!!", Toast.LENGTH_LONG).show();
-            }
-        }
-        else{
-            Toast.makeText(getBaseContext(),""  , Toast.LENGTH_LONG).show();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,(LocationListener) this);
     }
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
-        TextView T1= (TextView)findViewById(R.id.lat);
-        TextView T2= (TextView)findViewById(R.id.lon);
-
-        T1.setText(""+ location.getLatitude());
-        T2.setText(""+ location.getLongitude());
-
+        tv_lat.setText(Double.toString(location.getLatitude()));
+        tv_log.setText(Double.toString(location.getLongitude()));
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
+
     }
+
     @Override
     public void onProviderEnabled(@NonNull String provider) {
 
     }
+
     @Override
     public void onProviderDisabled(@NonNull String provider) {
-    }
 
+    }
 }
