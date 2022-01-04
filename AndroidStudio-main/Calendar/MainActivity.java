8cfb3f060d3caf3cf3f4400528b7bc97bf5bc424
true.living <68152189+swetha4444@users.noreply.github.com>
2022-01-05 00:10:59 +0530

    Add files via upload

---
+++ AndroidStudio-main/Calendar/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.calendar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.example.calendar.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.simple:
+                Toast.makeText(this, "s1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.simple1:
+                Toast.makeText(this, "s2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.simple2:
+                Toast.makeText(this, "s3", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+}
